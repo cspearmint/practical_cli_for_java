b45a7cf1a62a83bc4b5d27a0e10a93db92eabf5d
ale-leon <leon.alejandro@ufl.edu>
2024-04-22 20:23:23 -0400

    Document Argument class

--- src/main/java/oop/project/cli/Argument.java
+++ src/main/java/oop/project/cli/Argument.java
@@ -12,13 +12,25 @@ public class Argument {
         this.flag = flag;
     }
 
+    /**
+     * Get argument name
+     * @return String representing argument name
+     */
     String getName() {
         return this.name;
     }
 
+    /**
+     * Get argument type
+     * @return String representing argument type
+     */
     String getType() {
         return this.type;
     }
 
+    /**
+     * Determine whether argument is a flag
+     * @return Boolean being true if and only if argument is a flag
+     */
     boolean isFlag() { return this.flag; }
 }
